bcaa1531e685fdac7013c15cf7b9d69a19199e07
miriaamaral <miriaamaralcustodio@gmail.com>
2026-01-02 21:36:34 -0300

    feat: implementa cálculo de IMC com switch case

--- index.js
+++ index.js
@@ -18,3 +18,33 @@ Entre 25 e 29,99   | Acima do peso
 Entre 30 e 34,99   | Obesidade I
 Entre 35 e 39,99   | Obesidade II
 */
+
+const peso = 75;
+const altura = 1.7;
+const imc = Number((peso / (altura * altura)).toFixed(2));
+
+switch (true) {
+    case imc < 17:
+        console.log("Muito abaixo do peso");
+        break;
+
+    case imc >= 17 && imc <= 18.49:
+        console.log("Abaixo do peso");
+        break;
+
+    case imc >= 18.5 && imc <= 24.99:
+        console.log("Peso normal");
+        break;
+
+    case imc >= 25 && imc <= 29.99:
+        console.log("Acima do peso");
+        break;
+
+    case imc >= 30 && imc <= 34.99:
+        console.log("Obesidade I");
+        break;
+
+    case imc >= 35 && imc <= 39.99:
+        console.log("Obesidade II");
+        break;
+}
