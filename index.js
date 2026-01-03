dec8351a493319e946ae48d995f65dcf52b8de20
miriaamaral <miriaamaralcustodio@gmail.com>
2026-01-02 21:23:22 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+IMC é a sigla para Índice de Massa Corpórea,
+parâmetro adotado pela Organização Mundial
+de Saúde para calcular o peso ideal de cada pessoa.
+
+Como calcular o IMC?
+O IMC é calculado dividindo o peso (em kg) pela
+altura ao quadrado (em m), de acordo com a seguinte
+fórmula: IMC = peso / (altura x altura).
+
+E nosso calculo devemos retornar os seguintes valores para o usuário:
+
+Resultado do Calc  | Resultado para o usuário
+Abaixo de 17       | Muito abaixo do peso
+Entre 17 e 18,49   | Abaixo do peso
+Entre 18,5 e 24,99 | Peso normal
+Entre 25 e 29,99   | Acima do peso
+Entre 30 e 34,99   | Obesidade I
+Entre 35 e 39,99   | Obesidade II
+*/
